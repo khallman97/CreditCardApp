9f17bd7a18a0c049eddcc594806b3f456321959c
Kyle Hallman <khallman@my.yorku.ca>
2019-06-04 22:53:19 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:credit_card/HomePage.dart';
+
+void main() {
+ // Routes.initRoutes();
+  runApp(new MaterialApp(
+    title: "Credit Card",
+    home: new HomePage(),
+  ));
+}
